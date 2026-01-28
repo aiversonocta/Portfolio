2bb48e004f751255d4a66bcef1284aa1d6ce7b49
Aiverson Guila <guilaiverson@gmail.com>
2026-01-28 23:10:30 +0200

    Whatsapp integrado

--- Portfolio/menu.js
+++ Portfolio/menu.js
@@ -10,3 +10,22 @@ document.querySelectorAll(".menu a").forEach((link) => {
     menu.classList.remove("active");
   });
 });
+
+const btn = document.getElementById("sendWhatsapp");
+
+btn.addEventListener("click", () => {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+
+  const phone = "258841915701"; // teu WhatsApp (com código de Moçambique)
+
+  const text = `Olá Aiverson!%0A
+Nome: ${name}%0A
+Email: ${email}%0A
+Mensagem: ${message}`;
+
+  const url = `https://wa.me/${phone}?text=${text}`;
+
+  window.open(url, "_blank");
+});
---
+++ js/script.js
@@ -0,0 +1,14 @@
+const nome = "Aiverson";
+
+const idade = 20;
+
+let comidaFaorita;
+comidaFaorita = "Salada";
+
+let cidade;
+let pais;
+let bairro;
+let rua;
+let carro;
+
+console.log(nome, idade, comidaFaorita, cidade, pais, bairro, rua, carro);
